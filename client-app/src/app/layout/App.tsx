0ede35aba4ce5814e80a40e45075f69d92ae38e2
DritiDecani <ddecani@gmail.com>
2022-04-30 19:20:17 +0200

    Dashboard created--ActivityList

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { Container, List, ListItem } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 import { Activity } from "../models/activity";
 import NavBar from "./navbar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -15,17 +16,11 @@ function App() {
 
   return (
     <>
-      <NavBar/>
-      <Container style={{marginTop:'7em'}}>
-      <List>
-        {activities.map(activity => (
-          <ListItem key={activity.id}>
-            {activity.title}
-            </ListItem>
-        ))}
-      </List>
+      <NavBar />
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard activities={activities} />
       </Container>
-     
+
     </>
   );
 }
