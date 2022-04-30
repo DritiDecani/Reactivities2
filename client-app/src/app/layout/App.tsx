b2fa7eefd4e0f934cf9792c390a4bcc68d05379b
DritiDecani <ddecani@gmail.com>
2022-04-30 18:03:42 +0200

    Added navbar + some styling

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Container, List, ListItem } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import NavBar from "./navbar";
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get<Activity[]>("http://localhost:5000/api/activities").then((response) => {
+      setActivities(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <NavBar/>
+      <Container style={{marginTop:'7em'}}>
+      <List>
+        {activities.map(activity => (
+          <ListItem key={activity.id}>
+            {activity.title}
+            </ListItem>
+        ))}
+      </List>
+      </Container>
+     
+    </>
+  );
+}
+
+export default App;
