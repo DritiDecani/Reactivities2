a2bb053a2cda469ff719c5ca2252e50b709e5315
FatlindKelmendii <fk52131@ubt-uni.net>
2022-06-13 12:18:15 +0200

    add observer

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,14 +1,12 @@
+import { observer } from "mobx-react-lite";
 import React, { SyntheticEvent, useState } from "react";
 import { Button, Item, ItemContent, ItemDescription, Label, Segment } from "semantic-ui-react";
-import { Activity } from "../../../app/models/activity";
+import { useStore } from "../../../app/stores/store";
 
-interface Props {
-    activities: Activity[];
-    selectActivity: (id: string) => void;
-    deleteActivity: (id:string) => void;
-    submitting: boolean;
-}
-export default function ActivityList({ activities, selectActivity,deleteActivity , submitting}: Props) {
+export default observer(function ActivityList() {
+    const {activityStore} = useStore();
+    const {deleteActivity, activitiesByDate, loading} = activityStore;
+
     const [target , setTarget] = useState('');
 
 function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement> , id: string){
@@ -19,7 +17,7 @@ function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement> , id: string)
     return (
         <Segment>
             <Item.Group divided>
-                {activities.map(activity=>(
+                {activitiesByDate.map(activity=>(
                     <Item key={activity.id}>
                         <ItemContent>
                             <Item.Header as='a'>{activity.title}</Item.Header>
@@ -29,10 +27,10 @@ function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement> , id: string)
                                 <div>{activity.city},{activity.venue}</div>
                             </ItemDescription>
                             <Item.Extra>
-                                <Button onClick={() => selectActivity(activity.id)} floated="right" content='View' color="blue"/>
+                                <Button onClick={() => activityStore.selectActivity(activity.id)} floated="right" content='View' color="blue"/>
                                 <Button
                                 name={activity.id}
-                                loading={submitting && target === activity.id}
+                                loading={loading && target === activity.id}
                                 onClick={(e) => handleActivityDelete(e, activity.id)} 
                                 floated="right" 
                                 content='Delete' 
@@ -45,4 +43,4 @@ function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement> , id: string)
             </Item.Group>
         </Segment>
     );
-}
+})
