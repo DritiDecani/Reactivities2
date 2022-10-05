d5227024e6254ea739373c522db0fa9498fa7016
DritiDecani <ddecani@gmail.com>
2022-10-05 22:34:37 +0200

    //Production

--- client-app/src/app/stores/commentStore.ts
+++ client-app/src/app/stores/commentStore.ts
@@ -14,7 +14,7 @@ export default class CommentStore {
     createHubConnection = (actvivityId: string) =>{
         if(store.activityStore.selectedActivity){
             this.hubConnection = new HubConnectionBuilder()
-                .withUrl("http://localhost:5000/chat?activityId=" + actvivityId,{
+                .withUrl(process.env.REACT_APP_CHAT_URL + "?activityId=" + actvivityId,{
                     accessTokenFactory: () => store.userStore.user?.token!
                 })
                 .withAutomaticReconnect()
